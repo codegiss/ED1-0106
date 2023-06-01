6e53f5110587a6d6b8094e33c39328dbb55f0727
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-06-01 20:19:35 -0300

    Add files via upload

---
+++ bs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void print(int t, int *a) {
+	cout << "----------------------" << endl;
+	for(int i=0; i<t; ++i) {
+		cout << "Elemento: " << i << " = " << a[i] << endl;
+	}
+	cout << "----------------------" << endl;
+}
+
+void bubble_sort(int t, int *a) {
+	int aux;
+	for(int i=t-1; i>0; --i) {
+		for(int j=0; j<i; ++j) {
+			cout << "[" << j << "]: " << a[j] << 
+			        ", [" << j+1 << "]:  " << a[j+1];
+			if (a[j] > a[j+1]) {
+				aux = a[j];
+				a[j] = a[j+1];
+				a[j+1] = aux;
+				cout << " - Trocou!!!";
+			}
+			cout << endl;
+		}
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	int v[8] = {49, 38, 58, 87, 34, 93, 26, 13};
+	print(8, v);
+	bubble_sort(8, v);
+	print(8, v);
+	return 0;
+}
